44fdf9c541a9af6367e761f0b5cf6142dd4cf1a0
Jens Kristoffersen <jensakrr@gmail.com>
2021-02-08 21:23:29 +0100

    Create picker for date and time in AddPage

--- src/Add/AddPage.tsx
+++ src/Add/AddPage.tsx
@@ -4,21 +4,41 @@ import "./AddPage.css";
 import { Button } from "react-bootstrap";
 import { BsPlus } from "react-icons/bs";
 import PaceSlider from "./PaceSlider";
+import DateFnsUtils from "@date-io/date-fns";
+import { DateTimePicker, MuiPickersUtilsProvider } from "@material-ui/pickers";
+import { MaterialUiPickersDate } from "@material-ui/pickers/typings/date";
 
 const AddPage: FunctionComponent = (): ReactElement => {
   const [pace, setPace] = useState({ min: "5:00", max: "6:00" });
+  const [date, setDate] = useState(new Date());
+
   const handlePaceSliderChange = (min: string, max: string) => {
     setPace({ min, max });
-    console.log(pace.max);
+  };
+
+  const handleDateChanged = (value: MaterialUiPickersDate): void => {
+    if (value !== null) {
+      setDate(new Date(value.getDate()));
+    }
   };
 
   return (
     <div id={"add-container"}>
-      <FormItem title={"Time"}>16:30</FormItem>
+      <FormItem title={"Time"}>
+        <MuiPickersUtilsProvider utils={DateFnsUtils}>
+          <DateTimePicker
+            fullWidth
+            id={"date-time-picker"}
+            value={new Date()}
+            disablePast
+            onChange={handleDateChanged}
+          />
+        </MuiPickersUtilsProvider>
+      </FormItem>
       <FormItem title={"Pace (m/km)"}>
         <PaceSlider onChange={handlePaceSliderChange} />
       </FormItem>
-      <FormItem title={"Length"}>8-10km</FormItem>
+      <FormItem title={"Length (km)"}>8-10km</FormItem>
       <Button variant={"success"} size={"lg"} id={"add-run-button"}>
         <BsPlus size={"30"} />
         Add Run
